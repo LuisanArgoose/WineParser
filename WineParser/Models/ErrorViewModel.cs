47cfcfac311374bfb54f0cc9fcad220148b1a22c
Luzan <overcooked-luzan@example.com>
2024-08-26 12:19:55 +0300

    Добавьте файлы проекта.

---
+++ WineParser/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WineParser.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
